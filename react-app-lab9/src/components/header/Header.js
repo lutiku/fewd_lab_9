eebb114c3761c5d354185484d02ab6d8218ea099
lutiku <lutiku.ndongala@edu.devinci.fr>
2020-11-23 19:14:29 +0100

    Update Header.js

--- react-app-lab9/src/components/header/Header.js
+++ react-app-lab9/src/components/header/Header.js
@@ -1,18 +1,57 @@
 import React, { Component } from 'react' ;
+import Style from './Header.module.css'
 
 
 //import {NavLink} from "react-router-dom" ;
 
 
+class ClickButton extends Component{
+    constructor() {
+        super();
+        this.state = {
+            clickNum: 0
+        };
+        this.handleClicks = this.handleClicks.bind(this);
+    }
+    handleClicks(){
+        this.setState(prevState => ({
+            clickNum: prevState.clickNum + 1
+        }));
+    }
+
+    render() {
+        return(
+
+                <section className={"hero is-medium is-black is-bold" + Style.back} onClick={this.handleClicks}>
+                    <div className="hero-body">
+                        <div className="container">
+                            <h1 className="title">
+                                Articles and tips
+                            </h1>
+                            <h2 className="subtitle">
+                                The banner is clicked {this.state.clickNum} time(s).
+                            </h2>
+                        </div>
+                    </div>
+                </section>
+
+
+
+
+
+        );
+    }
+}
+
+
 export default class Header extends Component{
     render() {
         return(
-            <header className="navbar navbar-expand-lg navbar-light bg-light">
-
+            <>
                 <nav className="navbar" role="navigation" aria-label="main navigation">
                     <div className="navbar-brand">
                         <a className="navbar-item" href="https://bulma.io">
-                            <img src="https://bulma.io/images/bulma-logo.png" width="112" height="28"/>
+                            <p className="title">Bulma</p>
                         </a>
 
                         <a role="button" className="navbar-burger burger" aria-label="menu" aria-expanded="false"
@@ -25,25 +64,62 @@ export default class Header extends Component{
 
                     <div id="navbarBasicExample" className="navbar-menu">
                         <div className="navbar-start">
+                            <a className="navbar-item">
+                                Home
+                            </a>
 
+                            <a className="navbar-item">
+                                Documentation
+                            </a>
 
+                            <div className="navbar-item has-dropdown is-hoverable">
+                                <a className="navbar-link">
+                                    More
+                                </a>
 
+                                <div className="navbar-dropdown">
+                                    <a className="navbar-item">
+                                        About
+                                    </a>
+                                    <a className="navbar-item">
+                                        Jobs
+                                    </a>
+                                    <a className="navbar-item">
+                                        Contact
+                                    </a>
+                                    <hr className="navbar-divider"/>
+                                        <a className="navbar-item">
+                                            Report an issue
+                                        </a>
+                                </div>
+                            </div>
                         </div>
 
                         <div className="navbar-end">
                             <div className="navbar-item">
                                 <div className="buttons">
-                                    <a className="button is-primary">
-                                        <strong>Contact</strong>
+                                    <a className="button is-black">
+                                        <strong>Sign up</strong>
                                     </a>
-
+                                    <a className="button is-light">
+                                        Log in
+                                    </a>
                                 </div>
                             </div>
                         </div>
                     </div>
                 </nav>
 
-            </header>
+
+       <ClickButton/>
+
+
+
+        </>
+
+
+
+
 
 
 
