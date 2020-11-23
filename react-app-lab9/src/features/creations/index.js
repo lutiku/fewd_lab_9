fcf15f388d368e2a48450e942d424b6dfad22d9e
lutiku <lutiku.ndongala@edu.devinci.fr>
2020-11-23 19:14:20 +0100

    Create index.js

---
+++ react-app-lab9/src/features/creations/index.js
@@ -0,0 +1,19 @@
+import React from "react";
+import {CreationList} from "./components";
+
+
+const Creations = () =>{
+    return(
+        <>
+            <div className="d-flex flex-wrap justify-content-center flex-direction-column pt-5 mb-5">
+
+            <CreationList/>
+            </div>
+        </>
+    )
+}
+
+
+
+
+export default Creations
