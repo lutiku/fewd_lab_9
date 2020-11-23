3e711a5c3927d118fbe1d2a6242ecc85e5d90044
lutiku <lutiku.ndongala@edu.devinci.fr>
2020-11-23 19:16:55 +0100

    Update Footer.js

--- react-app-lab9/src/components/footer/Footer.js
+++ react-app-lab9/src/components/footer/Footer.js
@@ -12,10 +12,7 @@ export default class Footer extends Component{
             <footer className="footer">
                 <div className="content has-text-centered">
                     <p>
-                        <strong>Bulma</strong> by <a href="https://jgthms.com">Jeremy Thomas</a>. The source code is
-                        licensed
-                        <a href="http://opensource.org/licenses/mit-license.php">MIT</a>. The website content
-                        is licensed <a href="http://creativecommons.org/licenses/by-nc-sa/4.0/">CC BY NC SA 4.0</a>.
+                        <strong>Bulma</strong> by <a href="https://github.com/lutiku">Lutiku Ndongala</a>. 
                     </p>
                 </div>
             </footer>
